ec74e1270ff022c2f1eda05375c73d8260a3cbe7
IsaacDevSilva <isaacferreirasilva18@gmail.com>
2026-05-14 10:48:59 -0300

    criaçao dos botoes
    isaac, falta implementar as texturas de tudo, mas o basico funciona

--- Fuck Scripts/tela_inicial.gd
+++ Fuck Scripts/tela_inicial.gd
@@ -12,6 +12,12 @@ func _process(delta: float) -> void:
 func _on_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas +69/Mundo.tscn")
 
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://Cenas +69/creditos.tscn")
 
-func _on_catalogo_pressed() -> void:
+func _on_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas +69/catalogo.tscn")
+
+
+func _on_configuraçoes_pressed() -> void:
+	get_tree().change_scene_to_file("res://Cenas +69/configuraçao.tscn")

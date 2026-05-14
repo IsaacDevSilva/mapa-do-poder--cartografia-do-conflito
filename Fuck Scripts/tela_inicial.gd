369b693efb142ee409263cf923de77b24e229717
PEDRO LUIS TEMPONI MOREIRA <pedro.ltempo@gmail.com>
2026-05-14 11:36:13 -0300

    Tela do tutorial feito
    falta fazer a segunda pagina do tutorial

--- Fuck Scripts/tela_inicial.gd
+++ Fuck Scripts/tela_inicial.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 func _ready() -> void:
-	pass
+	$tutorial_panel_01.visible = false
+	$tutorial_panel_02.visible = false
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +18,25 @@ func _on_creditos_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas +69/creditos.tscn")
 
 func _on_tutorial_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas +69/catalogo.tscn")
-
-
+	$painel_principal.visible = false
+	$tutorial_panel_01.visible = true
 func _on_configuraçoes_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas +69/configuraçao.tscn")
+	pass
+func _on_proxima_pag_pressed() -> void:
+	$tutorial_panel_01.visible = false
+	$painel_principal.visible = false
+	$tutorial_panel_02.visible = true
+
+func _on_voltar_principal_01_pressed() -> void:
+	$tutorial_panel_01.visible = false
+	$painel_principal.visible = true
+	$tutorial_panel_02.visible = false
+func _on_voltar_catalogo_pressed() -> void:
+	$tutorial_panel_01.visible = true
+	$painel_principal.visible = false
+	$tutorial_panel_02.visible = false
+func _on_voltar_principal_02_pressed() -> void:
+	$tutorial_panel_01.visible = false
+	$painel_principal.visible = true
+	$tutorial_panel_02.visible = false
+	

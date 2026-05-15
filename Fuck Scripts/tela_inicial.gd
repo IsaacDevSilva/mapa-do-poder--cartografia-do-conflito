25ce1f16ae29884cef48ffe813257c03ed2ff309
IsaacDevSilva <isaacferreirasilva18@gmail.com>
2026-05-14 22:12:50 -0300

    animaçao completa do pergaminho
    talvez precise de um sprite proprio para o pergaminho, mecanica de escolha por script criada

--- Fuck Scripts/tela_inicial.gd
+++ Fuck Scripts/tela_inicial.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+var pagina_escolhida = ""
+var tutorial_rodando = false
 func _ready() -> void:
 	$tutorial_panel_01.visible = false
 	$tutorial_panel_02.visible = false
@@ -8,18 +9,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	escolher_texto_do_pergaminho()
 
 
 func _on_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas +69/Mundo.tscn")
 
 func _on_creditos_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas +69/creditos.tscn")
 
+	pagina_escolhida = "creditos"
 func _on_tutorial_pressed() -> void:
-	$painel_principal.visible = false
+
+	pagina_escolhida = "tutorial"
 	$tutorial_panel_01.visible = true
+	
 func _on_configuraçoes_pressed() -> void:
 	pass
 func _on_proxima_pag_pressed() -> void:
@@ -28,15 +31,39 @@ func _on_proxima_pag_pressed() -> void:
 	$tutorial_panel_02.visible = true
 
 func _on_voltar_principal_01_pressed() -> void:
-	$tutorial_panel_01.visible = false
-	$painel_principal.visible = true
-	$tutorial_panel_02.visible = false
-func _on_voltar_catalogo_pressed() -> void:
-	$tutorial_panel_01.visible = true
-	$painel_principal.visible = false
-	$tutorial_panel_02.visible = false
-func _on_voltar_principal_02_pressed() -> void:
-	$tutorial_panel_01.visible = false
-	$painel_principal.visible = true
-	$tutorial_panel_02.visible = false
 	
+	$painel_principal.visible = true
+
+	$"animaçao pergaminho".play_backwards("animaçao tutorial")
+
+	
+func _on_voltar_principal_02_pressed() -> void:
+	$"animaçao pergaminho".play_backwards("animaçao tutorial")
+	
+	tutorial_rodando = false
+	
+	$painel_principal.visible = true
+
+
+	
+func escolher_texto_do_pergaminho():
+	if pagina_escolhida == "":
+		pass
+	elif pagina_escolhida == "tutorial" and not tutorial_rodando:
+		$"animaçao pergaminho".play("animaçao tutorial")
+		tutorial_rodando = true
+	elif pagina_escolhida == "creditos":
+		$"animaçao pergaminho".play()
+
+
+func _on_voltar_catalogo_pressed() -> void:
+	$tutorial_panel_02.visible = false
+	$tutorial_panel_01.visible = true
+
+
+func _on_animaçao_pergaminho_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "animaçao tutorial":
+		pagina_escolhida = ""
+		tutorial_rodando = false
+		
+		

c24a61c99a3bca95b1c5353028683a35d1591cba
IsaacDevSilva <isaacferreirasilva18@gmail.com>
2026-05-24 17:16:40 -0300

    menu estastisitcas na tela mundo
    melhorar visual

--- Fuck Scripts/mundo.gd
+++ Fuck Scripts/mundo.gd
@@ -2,6 +2,7 @@ extends Node2D
 var botao
 var vbox
 var pais = null
+var estatisticas_aberto = false
 
 func _ready() -> void:
 	for vbox in $escolha.get_children():
@@ -13,9 +14,33 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if pais != null:
-		print(paises.paises[pais]["pib"])
+		$pais_nome.text = pais
 func _botao_clicado(botao):
 	pais = botao.name
 	$escolha.visible = false
 	print(botao)
 	print(pais)
+
+
+func _on_estatisticas_pressed() -> void:
+	if estatisticas_aberto == false:
+		$"animaçoes_geral".play("estatisticas")
+		estatisticas_aberto = true
+		var texto = ""
+
+		for chave in paises.paises[pais]:
+			var valor = paises.paises[pais][chave]
+
+			if chave == "pib":
+				valor = str(valor) + " T"
+
+			elif chave == "populacao":
+				valor = str(valor) + " M"
+
+			texto += str(chave) + ": " + str(valor) + "\n"
+
+		$"estatisticas_informaçoes".text = texto
+		
+	elif estatisticas_aberto:
+		$"animaçoes_geral".play_backwards("estatisticas")
+		estatisticas_aberto = false

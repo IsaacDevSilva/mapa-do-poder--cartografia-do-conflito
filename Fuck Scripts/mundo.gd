94d65202d12a81419693c21c6613ba73142ccbb9
IsaacDevSilva <isaacferreirasilva18@gmail.com>
2026-05-25 09:07:46 -0300

    mecanica de turnos criada
    script + sprites

--- Fuck Scripts/mundo.gd
+++ Fuck Scripts/mundo.gd
@@ -3,6 +3,10 @@ var botao
 var vbox
 var pais = null
 var estatisticas_aberto = false
+var data
+var dia_atual 
+var mes_atual
+var ano_atual = 2025
 
 func _ready() -> void:
 	for vbox in $escolha.get_children():
@@ -15,6 +19,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if pais != null:
 		$pais_nome.text = pais
+		
 func _botao_clicado(botao):
 	pais = botao.name
 	$escolha.visible = false
@@ -27,20 +32,33 @@ func _on_estatisticas_pressed() -> void:
 		$"animaçoes_geral".play("estatisticas")
 		estatisticas_aberto = true
 		var texto = ""
-
 		for chave in paises.paises[pais]:
 			var valor = paises.paises[pais][chave]
-
 			if chave == "pib":
 				valor = str(valor) + " T"
-
 			elif chave == "populacao":
 				valor = str(valor) + " M"
-
 			texto += str(chave) + ": " + str(valor) + "\n"
-
 		$"estatisticas_informaçoes".text = texto
-		
 	elif estatisticas_aberto:
 		$"animaçoes_geral".play_backwards("estatisticas")
 		estatisticas_aberto = false
+		
+func _turnos(dia_atual,mes_atual,ano_atual):
+	var meses = [
+		"janeiro", "fevereiro", "março", "abril",
+		"maio", "junho", "julho", "agosto",
+		"setembro", "outubro", "novembro", "dezembro"
+		]
+	mes_atual = meses[mes_atual - 1]
+	
+	$dia.text = str(dia_atual)
+	$mes.text = str(mes_atual)
+	$ano.text = str(ano_atual)
+
+func _on_conflitar_pressed() -> void:
+	dia_atual = int(randi_range(1,30))
+	mes_atual = int(randi_range(1,12))
+	ano_atual = int(ano_atual+ 1)
+	_turnos(dia_atual,mes_atual,ano_atual)
+	
